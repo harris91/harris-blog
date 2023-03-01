1f56b84a8d3b01f35a23a0e982a5615e60902fa2
BBAGA91 <57648788+BBAGA91@users.noreply.github.com>
2023-03-02 01:04:48 +0900

    fix(UI): PostFooter 수정 - tags 추가 - 피드백 문구 추가

--- src/containers/Detail/components/PostDetail/PostFooter.tsx
+++ src/containers/Detail/components/PostDetail/PostFooter.tsx
@@ -1,29 +1,48 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import Tag from "@components/Tag"
+import { TPost } from "@/src/types"
 
-type Props = {}
+type Props = {
+  data: TPost
+}
 
-const Footer: React.FC<Props> = () => {
+const Footer: React.FC<Props> = ({ data }) => {
   const router = useRouter()
   return (
-    <div className="flex justify-between font-medium text-gray-500 dark:text-gray-400">
-      <span>
-        <button
-          onClick={() => router.push('/')}
-          className="mt-2 cursor-pointer hover:text-black dark:hover:text-gray-100"
-        >
-          ← Back
-        </button>
-      </span>
-      <span>
-        <button
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-          className="mt-2 cursor-pointer hover:text-black dark:hover:text-gray-100"
-        >
-          ↑ Top
-        </button>
-      </span>
-    </div>
+
+    <>
+      <p className="text-center text-gray-500 dark:text-gray-400 mt-8 mb-2 py-12 bg-gray-100 dark:bg-gray-800">
+        긴 글 읽어주셔서 감사합니다. <br/>
+        오탈자 및 내용 피드백은 언제나 환영합니다.
+      </p>
+      {data.tags && (
+        <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags gap-2 mb-4">
+          {data.tags.map((tag: string) => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </div>
+      )}
+
+      <div className="flex justify-between font-medium text-gray-500 dark:text-gray-400">
+        <span>
+          <button
+            onClick={() => router.push('/')}
+            className="mt-2 cursor-pointer hover:text-black dark:hover:text-gray-100"
+          >
+            ← Back
+          </button>
+        </span>
+        <span>
+          <button
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+            className="mt-2 cursor-pointer hover:text-black dark:hover:text-gray-100"
+          >
+            ↑ Top
+          </button>
+        </span>
+      </div>
+    </>
   )
 }
 
