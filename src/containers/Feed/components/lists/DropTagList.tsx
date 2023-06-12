a48accbbea7cd4e9d49f8add4d894eb854ba82cc
harris91 <flqlfn@gmail.com>
2023-06-12 09:11:53 +0900

    style: 코드라인 / 태그리스트 배경색 변경

--- src/containers/Feed/components/lists/DropTagList.tsx
+++ src/containers/Feed/components/lists/DropTagList.tsx
@@ -1,5 +1,5 @@
 import { TTags } from '@custeomTypes/index'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { Menu, Transition } from '@headlessui/react'
 
@@ -22,15 +22,15 @@ const DropTagList: React.FC<Props> = ({ className, data }) => {
       },
     })
   }
-  
+
   const [selectedTag, setSelectedTag] = useState(currentTag)
   return (
     <div className={className}>
       <Menu as="div" className="w-full inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center focus:outline-none focus-visible:ring-1 focus-visible:ring-white focus-visible:ring-opacity-75
-          rounded-md px-4 py-2 text-sm font-medium 
-          text-sky-500 border border-[1.5px] border-sky-500 hover:bg-sky-500 hover:text-white
+          <Menu.Button className="inline-flex w-full justify-center focus:outline-none focus-visible:ring-1 
+          focus-visible:ring-white focus-visible:ring-opacity-75 rounded-md px-4 py-2 text-sm font-medium 
+          text-sky-500 border-[1.5px] border-sky-500 hover:bg-sky-500 hover:text-white
           dark:bg-sky-500 dark:border-0 dark:py-2.5 dark:text-white dark:bg-opacity-60 dark:hover:bg-opacity-80">
             #{showTag(currentTag)}
           </Menu.Button>
@@ -44,33 +44,34 @@ const DropTagList: React.FC<Props> = ({ className, data }) => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute overflow-auto h-auto min-w-[150px] max-h-96 z-10 left-0 mt-2 w-full rounded-md border border-black 
-            dark:border-white border-opacity-20 dark:border-opacity-10 bg-opacity-80 backdrop-blur-lg focus-visible:ring">
-          <div className="px-1 py-1">
-            {Object.keys(data).map((key) => (
-              <Menu.Item key={key}>
-              {({ active }) => (
-                <button
-                  className={`
+          <Menu.Items className="absolute overflow-auto h-auto min-w-[150px] max-h-96 z-10 left-0 mt-2 w-full 
+            rounded-md border border-black dark:border-white border-opacity-20 dark:border-opacity-10 
+            bg-zinc-900 bg-opacity-70 backdrop-blur-xl focus-visible:ring">
+            <div className="px-1 py-1">
+              {Object.keys(data).map((key) => (
+                <Menu.Item key={key}>
+                  {({ active }) => (
+                    <button
+                      className={`
                     group flex w-full items-center rounded-sm px-2 py-2 text-sm
-                    ${ active ? 'bg-gray-400 bg-opacity-30 dark:text-white' : 'dark:text-white'} 
-                    ${ key === currentTag &&'text-sky-600 dark:text-sky-600 font-bold' }
+                    ${active ? 'bg-gray-400 bg-opacity-30 dark:text-white' : 'dark:text-white'} 
+                    ${key === currentTag && 'text-sky-600 dark:text-sky-600 font-bold'}
                   `}
-                  onClick={() => handleClickTag(key)}
-                >
-                  {key}
-                  {key === currentTag && (
-                    <span className="absolute right-0 flex items-center pr-4 ">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="h-5 w-5">
-                        <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd"></path>
-                      </svg>
-                    </span>
+                      onClick={() => handleClickTag(key)}
+                    >
+                      {key}
+                      {key === currentTag && (
+                        <span className="absolute right-0 flex items-center pr-4 ">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="h-5 w-5">
+                            <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd"></path>
+                          </svg>
+                        </span>
+                      )}
+                    </button>
                   )}
-                </button>
-              )}
-              </Menu.Item>
-            ))}
-          </div>
+                </Menu.Item>
+              ))}
+            </div>
           </Menu.Items>
         </Transition>
       </Menu>
